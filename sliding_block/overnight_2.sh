0331088d380cbae8f85ec6dfd2cc7b80440ed9ad
sanjaythakur <sttsanjay@gmail.com>
2018-09-03 19:01:52 -0400

    GP adjustments for windows size = 1

--- sliding_block/overnight_2.sh
+++ sliding_block/overnight_2.sh
@@ -4,11 +4,6 @@ python Generate_GP_Controller.py -c 1 -ws 2 -po True -bc GP -tc LQR
 python Generate_GP_Controller.py -c 2 -ws 2 -po True -bc GP -tc LQR
 python Generate_GP_Controller.py -c 3 -ws 2 -po True -bc GP -tc LQR
 python Generate_GP_Controller.py -c 4 -ws 2 -po True -bc GP -tc LQR
-python Generate_GP_Controller.py -c 0 -ws 1 -po False -bc GP -tc LQR
-python Generate_GP_Controller.py -c 1 -ws 1 -po False -bc GP -tc LQR
-python Generate_GP_Controller.py -c 2 -ws 1 -po False -bc GP -tc LQR
-python Generate_GP_Controller.py -c 3 -ws 1 -po False -bc GP -tc LQR
-python Generate_GP_Controller.py -c 4 -ws 1 -po False -bc GP -tc LQR
 python Generate_GP_Controller.py -c 0 -ws 2 -po False -bc GP -tc LQR
 python Generate_GP_Controller.py -c 1 -ws 2 -po False -bc GP -tc LQR
 python Generate_GP_Controller.py -c 2 -ws 2 -po False -bc GP -tc LQR
